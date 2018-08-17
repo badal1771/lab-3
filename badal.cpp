f0a3cd167f2acb0c4bd779b91463c5362d30d756
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-08-17 06:46:34 +0000

    Add files via upload

---
+++ badal.cpp
@@ -0,0 +1,24 @@
+// including th elibrary
+#include<iostream>
+using namespace std ;
+//write the function
+int main(){
+//declaring the variables;
+int a, b, sum;
+float c, d; 
+//assing values
+a = 35;
+b = 40;
+c = 38;
+d =45;
+//process
+sum = a * b;
+cout <<sum <<endl;
+cout<<a *b <<endl;
+cout <<b % a <<endl;
+cout <<d /c <<endl;
+
+return 0;
+}
+
+
